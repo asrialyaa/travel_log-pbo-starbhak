02212df68d17f272065622b2d682c28feda15751
@Choirizanourma <nourmautami56@gmail.com>
2022-08-01 14:39:09 +0700

    dashboard & Crud

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -10,30 +11,91 @@ class LoginController extends Controller
         return view('login'); 
     }
 
-    public function registration(){ 
-        return view('registrasi'); 
+    public function registration()
+    {
+        return view('registrasi');
     }
-
-    public function postLogin(Request $request) { 
-        $request->validate([ 'nik' => 'required', 'namalengkap' => 'required', 'password' => 'required', ]); 
-        $credentials = $request->only('nik', 'namalengkap', 'password'); if (Auth::attempt($credentials)) 
-        { return redirect()->intended('dashboard') ->withSuccess('anda berhasi masuk'); } 
-        return redirect("login")->withSuccess('oh tidak! kamu tidak punya hak akses'); 
+      
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function postLogin(Request $request)
+    {
+        $request->validate([
+            'nik' => 'required',
+            'namalengkap' => 'required',
+            'password' => 'required',
+        ]);
+   
+        $credentials = $request->only('nik', 'namalengkap', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard')
+                        ->withSuccess('You have Successfully loggedin');
+        }
+  
+        return redirect("login")->withSuccess('Oppes! You have entered invalid credentials');
     }
-
-    public function postRegistration(Request $request) { 
-        $request->validate([ 'nik' => 'required', 'namalengkap' => 'required', 'email' => 'required|email|unique:users', 'password' => 'required|min:6', ]); $data = $request->all(); $check = $this->create($data); return redirect("dashboard")->withSuccess('hebat! kamu berhasil login'); 
+      
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function postRegistration(Request $request)
+    {  
+        $request->validate([
+            'nik' => 'required',
+            'namalengkap' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+           
+        $data = $request->all();
+        $check = $this->create($data);
+         
+        return redirect("dashboard")->withSuccess('Great! You have Successfully loggedin');
     }
-
-    public function dashboard() {
-        if(Auth::check()){ return view('dashboard'); } return redirect("login")->withSuccess('oh tidak! kamu tidak ada akses'); 
+    
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function dashboard()
+    {
+        if(Auth::check()){
+            return view('dashboard');
+        }
+  
+        return redirect("login")->withSuccess('Opps! You do not have access');
     }
-
-    public function create(array $data) {
-        return User::create([ 'nik' => $data['nik'], 'namalengkap' => $data['namalengkap'], 'email' => $data['email'], 'password' => Hash::make($data['password']) ]); 
+    
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function create(array $data)
+    {
+      return User::create([
+        'nik' => $data['nik'],
+        'namalengkap' => $data['namalengkap'],
+        'email' => $data['email'],
+        'password' => Hash::make($data['password'])
+      ]);
     }
-
-    public function logout() { 
-        Session::flush(); Auth::logout(); return Redirect('login'); 
+    
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function logout() {
+        Session::flush();
+        Auth::logout();
+  
+        return Redirect('login');
     }
 }
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <center>
+        <h1>Selamat Datang</h1>
+        <a href="{{ url('dashboard') }}" style="text-decoration:none;margin-left:.2rem;color:black">Dashboard</a>
+        <a href="{{ url('catatanperjalanan') }}" style="text-decoration:none;margin-left:.2rem;color:black">Catatan Perjalanan</a>
+        <a href="{{ url('logout') }}" style="text-decoration:none;margin-left:.2rem;color:black">Logout</a>
+        </center>
+    
+        <table class="table container mt-5"> 
+            <thead class="text-dark"> 
+              <tr> 
+                <th scope="col">No</th>  
+                <th scope="col">Date</th> 
+                <th scope="col">Time</th> 
+                <th scope="col">Location</th> 
+                <th scope="col">Body Temprature</th> 
+              </tr> 
+            </thead> 
+            <tbody>
+              <tr class="text-dark"> 
+                <td scope="col">1</td>
+                <td scope="col">18-02-2005</td>
+                <td scope="col">20.30</td>
+                <td scope="col">Jakarta</td>
+                <td scope="col">20</td> 
+              </tr> 
+              <tr class="text-dark"> 
+                <td scope="col">2</td>
+                <td scope="col">10-12-2020</td>
+                <td scope="col">10.40</td>
+                <td scope="col">Bandung</td>
+                <td scope="col">15</td> 
+              </tr> 
+              <button type="button" class="btn btn-primary mt-3" style="margin-left:8%">Tambah</button>
+            </tbody>      
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
